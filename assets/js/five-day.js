c4721b150323ee8ac45c56be8e80d4b61380e1d3
Shital Chaudhary <shitalchau10@gmail.com>
2021-08-05 23:47:59 -0500

    add dynamic html corrosponding to api data

--- assets/js/five-day.js
+++ assets/js/five-day.js
@@ -4,7 +4,7 @@ var forecast = document.getElementsByClassName("component-forecast-container")[0
 
 var apiForecastUrl = "https://api.openweathermap.org/data/2.5/forecast?q=Chicago&appid=33d588881cf1e072943e6745ea106abc";
 
-function getWeatherData () {
+function getWeatherData() {
 
     // fetch api
     fetch(apiForecastUrl).then((response) => {
@@ -24,7 +24,48 @@ function getWeatherData () {
 }
 getWeatherData()
 
-var displayForecastWeather = function(data){
-    currentLocation.textContent = data.list[0].dt;
+var displayForecastWeather = function (data) {
+
+    for (var i = 0; i < 5; i++) {
+        // Date
+        var forecastedDate = new Date(data.list[i].dt * 1000);
+        var dateEl = document.createElement("p");
+        dateEl.textContent = forecastedDate.toLocaleDateString();
+        
+        // Weather
+        var weatherIcon = JSON.stringify(data.list[i].weather[0].icon);
+        weatherIcon = weatherIcon.slice(1, -1);
+        weatherIcon = weatherIcon.split(" ")[0];
+        var weatherIconEl = document.createElement("img");
+        weatherIconEl.setAttribute("src", "http://openweathermap.org/img/wn/" + weatherIcon +"@2x.png");
+       
+        // Weather Tempreture
+        var weatherTempEl = document.createElement("p");
+        weatherTempEl.textContent = data.list[i].main.temp;
+
+        // Weather Description
+        var weatherDescriptionEl = document.createElement("p");
+        weatherDescriptionEl.textContent = data.list[i].weather[0].description;
+
+        // Wind
+        var weatherWindEl = document.createElement("p");
+        weatherWindEl.textContent = "Wind: " + data.list[i].wind.speed + " mphs";
+        
+        // Humidity
+        var weatherHumidityEl = document.createElement("p");
+        weatherHumidityEl.textContent = "Humidity: " + data.list[i].main.humidity + "%";
+
+        // gust
+        var weatherGustEl = document.createElement("p");
+        weatherGustEl.textContent = "Gust: " + data.list[i].wind.gust + " mphs";
+
+        currentStats.append(dateEl, weatherIconEl, weatherTempEl, weatherDescriptionEl, weatherWindEl, weatherHumidityEl, weatherGustEl);
+
+
+
+        // currentLocation.textContent = data.list[0].dt_txt;
+
+    }
+    console.log(currentStats);
 
 }
